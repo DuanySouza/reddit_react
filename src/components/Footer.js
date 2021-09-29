ad16ad8fa67ce3f60fce603829c056610dfd6d17
Duany Souza <duanysouza01@gmail.com>
2021-09-29 19:46:32 -0300

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'; 
+
+import '../styles/components/Footer.css';
+
+/**
+ * Component responsible for returning the footer
+ * 
+ * @returns {JSX.Element} footer element
+ */
+export default function Footer(){
+    return (
+        <footer>
+            <h4>Developed by Duany Souza</h4>
+        </footer>
+    )
+}
